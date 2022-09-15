7d2d220655812a419099d34bcf4c37bf8e5ed337
kirannarwade <90048456+kirannarwade@users.noreply.github.com>
2022-09-15 23:06:00 +0530

    Add files via upload
    add setup mvt

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def store(request):
+    context = {}
+    return render(request, "store.html", context)
+
+def cart(request):
+    context = {}
+    return render(request, "cart.html", context)
+
+def checkout(request):
+    context = {}
+    return render(request, "checkout.html", context)
